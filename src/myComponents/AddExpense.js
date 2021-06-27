b063852845eccb03186f090cc26ce66ed1103ce1
Kartik Koul <kartik.koul.01@gmail.com>
2021-06-27 22:15:10 +0530

    - Add Expense Component Styled 🚀

--- src/myComponents/AddExpense.js
+++ src/myComponents/AddExpense.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import './AddExpense.css'
 const AddExpense = () => {
+
+    const addNewExpense = (e) =>{
+        e.preventDefault();
+        console.log('$150k+/year is not so far!! 🚀')
+    }
+
     return (
         <div className="addExpenseOuter">
             <div className="addExpenseInner">
-                <button className="addExpenseButton">Add New Expense</button>
+                <form onSubmit={addNewExpense}>
+                    <div className="inputs">
+                        <input type="text" placeholder="TITLE"/>
+                        <input type="number" placeholder="Amount"/>
+                        <input type="date"/>
+                    </div>
+
+                    <div className="buttonArea">
+                    <button className="addExpenseButton" type='submit'>ADD NEW EXPENSE</button>
+                    </div>
+                    
+                </form>
             </div>
         </div>
     )
