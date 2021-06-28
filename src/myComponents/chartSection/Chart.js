7dab22213d20dc6311598bd2369465720442fed2
Kartik Koul <kartik.koul.01@gmail.com>
2021-06-28 23:27:26 +0530

    - Project Restuctured 🚀

--- src/myComponents/chartSection/Chart.js
+++ src/myComponents/chartSection/Chart.js
@@ -6,7 +6,20 @@ const Chart = () => {
     return (
         <div className="chartOuter">
             <div className="chartInner">
-                CHART GOES HERE!!
+
+                <div className="filterSection">
+                    <p>Filter by year</p>
+                    <select>
+                        <option value="2021">2021</option>
+                        <option value="2020">2020</option>
+                        <option value="2019">2019</option>
+                        <option value="2018">2018</option>
+                    </select>
+                </div>
+                
+                <div className="chartSection">
+                    CHART GOES HERE!!
+                </div>
             </div>
         </div>
     )
