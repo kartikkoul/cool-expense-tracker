17f3cfb81250c11b2c0c3a1405f3d9095f89855e
Kartik Koul <kartik.koul.01@gmail.com>
2021-06-29 13:12:27 +0530

    - Filter Year Value Derivable

--- src/myComponents/chartSection/Chart.js
+++ src/myComponents/chartSection/Chart.js
@@ -3,13 +3,21 @@ import './Chart.css'
 
 
 const Chart = () => {
+
+    let filterYear = '';
+
+    const filteredYear = e =>{
+        filterYear=e.target.value;
+    }
+
+
     return (
         <div className="chartOuter">
             <div className="chartInner">
 
                 <div className="filterSection">
                     <p>Filter by year</p>
-                    <select>
+                    <select onChange={filteredYear}>
                         <option value="2021">2021</option>
                         <option value="2020">2020</option>
                         <option value="2019">2019</option>
